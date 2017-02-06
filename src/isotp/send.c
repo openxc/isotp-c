aaf3310eb47b435e54e5c60e12febeb3fdc35c2b
Ali Hassani <ahassa37@ford.com>
2017-02-06 17:27:06 +0000

    expanded multi-frame write code to parse payload

--- src/isotp/send.c
+++ src/isotp/send.c
@@ -5,6 +5,8 @@
 #define PCI_NIBBLE_INDEX 0
 #define PAYLOAD_LENGTH_NIBBLE_INDEX 1
 #define PAYLOAD_BYTE_INDEX 1
+#define MULTI_PAYLOAD_BYTE_INDEX 2
+
 
 void isotp_complete_send(IsoTpShims* shims, IsoTpMessage* message,
         bool status, IsoTpMessageSentHandler callback) {
@@ -13,49 +15,7 @@ void isotp_complete_send(IsoTpShims* shims, IsoTpMessage* message,
     }
 }
 
-IsoTpSendHandle isotp_send_single_frame(IsoTpShims* shims, IsoTpMessage* message,
-        IsoTpMessageSentHandler callback) {
-    IsoTpSendHandle handle = {
-        success: false,
-        completed: true
-    };
-
-    uint8_t can_data[CAN_MESSAGE_BYTE_SIZE] = {0};
-    if(!set_nibble(PCI_NIBBLE_INDEX, PCI_SINGLE, can_data, sizeof(can_data))) {
-        shims->log("Unable to set PCI in CAN data");
-        return handle;
-    }
-
-    if(!set_nibble(PAYLOAD_LENGTH_NIBBLE_INDEX, message->size, can_data,
-                sizeof(can_data))) {
-        shims->log("Unable to set payload length in CAN data");
-        return handle;
-    }
-
-    if(message->size > 0) {
-        memcpy(&can_data[1], message->payload, message->size);
-    }
-
-    shims->send_can_message(message->arbitration_id, can_data,
-            shims->frame_padding ? 8 : 1 + message->size);
-    handle.success = true;
-    isotp_complete_send(shims, message, true, callback);
-    return handle;
-}
-
-IsoTpSendHandle isotp_send_multi_frame(IsoTpShims* shims, IsoTpMessage* message,
-        IsoTpMessageSentHandler callback) {
-    // TODO make sure to copy message into a local buffer
-    shims->log("Only single frame messages are supported");
-    IsoTpSendHandle handle = {
-        success: false,
-        completed: true
-    };
-    // TODO need to set sending and receiving arbitration IDs separately if we
-    // can't always just add 0x8 (and I think we can't)
-    return handle;
-}
-
+// Depending on CAN message length, calls appropriate send frame function
 IsoTpSendHandle isotp_send(IsoTpShims* shims, const uint16_t arbitration_id,
         const uint8_t payload[], uint16_t size,
         IsoTpMessageSentHandler callback) {
@@ -72,18 +32,171 @@ IsoTpSendHandle isotp_send(IsoTpShims* shims, const uint16_t arbitration_id,
     }
 }
 
-bool isotp_continue_send(IsoTpShims* shims, IsoTpSendHandle* handle,
+// Function to send single 8 byte CAN frame
+IsoTpSendHandle isotp_send_single_frame(IsoTpShims* shims, IsoTpMessage* message,
+        IsoTpMessageSentHandler callback) {
+    IsoTpSendHandle handle = {
+        success: false,
+        completed: true
+    };    
+	
+	uint8_t can_data[CAN_MESSAGE_BYTE_SIZE] = {0};
+	
+	// define the message header as single CAN frame (PCI_SINGLE) in first nibble
+    if(!set_nibble(PCI_NIBBLE_INDEX, PCI_SINGLE, can_data, sizeof(can_data))) {
+        shims->log("Unable to set PCI in CAN data");
+        return handle;
+    }
+
+	// define the total message size in the second nibble
+    if(!set_nibble(PAYLOAD_LENGTH_NIBBLE_INDEX, message->size, can_data,
+                sizeof(can_data))) {
+        shims->log("Unable to set payload length in CAN data");
+        return handle;
+    }
+
+	// if size is valid, copy the payload
+    if(message->size > 0) {
+        memcpy(&can_data[PAYLOAD_BYTE_INDEX], message->payload, message->size);
+    }
+
+	// send the CAN message and update handler to be true
+    shims->send_can_message(message->arbitration_id, can_data,
+            shims->frame_padding ? 8 : 1 + message->size);
+    handle.success = true;
+    isotp_complete_send(shims, message, true, callback);
+    return handle;
+}
+
+// Function to send First Frame of multi-frame CAN message
+IsoTpSendHandle isotp_send_multi_frame(IsoTpShims* shims, IsoTpMessage* message,
+        IsoTpMessageSentHandler callback) {
+	// note that we intentionally mark handle.completed as false to ensure UDS knows to continue sending
+    IsoTpSendHandle handle = {
+        success: false,
+        completed: false
+    };
+			
+	// create the buffer
+	uint8_t can_data[CAN_MESSAGE_BYTE_SIZE] = {0};
+	
+	// define the message header as first CAN frame (PCI_FIRST_FRAME) in first nibble
+    if(!set_nibble(PCI_NIBBLE_INDEX, PCI_FIRST_FRAME, can_data, sizeof(can_data))) {
+        shims->log("Unable to set PCI in first frame CAN data");
+        return handle;
+    }
+
+	// define the total message size (note, now we have 3 nibbles instead of 1)
+	if(!set_bitfield(message->size,4,12,can_data,sizeof(can_data))){
+        shims->log("Unable to set payload length in CAN data");
+        return handle;
+    }
+
+	// begin payload after byte 2, but only copy first 6
+    if(message->size > 0) {
+        memcpy(&can_data[MULTI_PAYLOAD_BYTE_INDEX], message->payload, 6);
+    }
+	
+	// send the CAN message and update handler to be true
+    shims->send_can_message(message->arbitration_id, can_data,
+            shims->frame_padding ? 8 : 1 + message->size);
+    handle.success = true;
+    isotp_complete_send(shims, message, true, callback);
+    return handle;
+}
+
+IsoTpSendHandle isotp_send_first_frame(IsoTpShims* shims, IsoTpSendHandle &handle, IsoTpMessage* message,
+        IsoTpMessageSentHandler callback) {
+	// create the buffer
+	uint8_t can_data[CAN_MESSAGE_BYTE_SIZE] = {0};
+	
+	// define the message header as first CAN frame (PCI_FIRST_FRAME) in first nibble
+    if(!set_nibble(PCI_NIBBLE_INDEX, PCI_FIRST_FRAME, can_data, sizeof(can_data))) {
+        shims->log("Unable to set PCI in first frame CAN data");
+        return handle;
+    }
+
+	// define the total message size (note, now we have 3 nibbles instead of 1)
+	if(!set_bitfield(message->size,4,12,can_data,sizeof(can_data))){
+        shims->log("Unable to set payload length in CAN data");
+        return handle;
+    }
+
+	// begin payload after byte 2, but only copy first 6
+    if(message->size > 0) {
+        memcpy(&can_data[MULTI_PAYLOAD_BYTE_INDEX], message->payload, 6);
+    }
+	
+	// send the CAN message and update handler to be true
+    shims->send_can_message(message->arbitration_id, can_data,
+            shims->frame_padding ? 8 : 1 + message->size);
+    handle.success = true;
+    isotp_complete_send(shims, message, true, callback);
+    return handle;
+}
+
+IsoTpSendHandle isotp_send_second_frame(IsoTpShims* shims, uint16_t frame_count, uint16_t num_frames, IsoTpSendHandle &handle,IsoTpMessage* message,
+        IsoTpMessageSentHandler callback) {
+
+	// reset the handle success value
+	handle.success = false;
+	
+	// create the buffer
+	uint8_t can_data[CAN_MESSAGE_BYTE_SIZE] = {0};
+	
+	// define the message header as second CAN frame (PCI_CONSECUTIVE_FRAME) in first nibble
+    if(!set_nibble(PCI_NIBBLE_INDEX, PCI_CONSECUTIVE_FRAME, can_data, sizeof(can_data))) {
+        shims->log("Unable to set PCI in second frame CAN data");
+        return handle;
+    }
+
+	// define which consecutive frame this is in the second nibble
+    if(!set_nibble(PAYLOAD_LENGTH_NIBBLE_INDEX, message->size, can_data,
+                sizeof(can_data))) {
+        shims->log("Unable to set second frame number in CAN data");
+        return handle;
+    }
+
+	// copy the payload and send the message
+	// note that payload reference is shifted by first 6 bits of first frame, and 7 for each consecutive
+	// last frame we only need to take remainder
+	uint16_t shift = 6+(count-1)*7;
+    if(frame_count < num_frames) {	
+		// begin payload after first byte, accounting for first frame has 6 bits and each second takes 7
+		memcpy(&can_data[1], message->payload + shift, 7); 
+		shims->send_can_message(message->arbitration_id, can_data, 8);
+    }
+	else{
+		// if we're on the final frame, we need to be careful to only take the remainder in the payload
+		memcpy(&can_data[1], message->payload + shift, message->size % shift); 		
+		shims->send_can_message(message->arbitration_id, can_data, 1 + message->size % shift);
+	}
+	
+	// send the CAN message and update handler to be true
+    handle.success = true;
+    isotp_complete_send(shims, message, true, callback);
+    return handle;
+}
+
+IsoTpSendHandle isotp_continue_send(IsoTpShims* shims, IsoTpSendHandle* handle,
         const uint16_t arbitration_id, const uint8_t data[],
         const uint8_t size) {
-    // TODO this will need to be tested when we add multi-frame support,
-    // which is when it'll be necessary to pass in CAN messages to SENDING
-    // handles.
-    if(handle->receiving_arbitration_id != arbitration_id) {
-        if(shims->log != NULL) {
-            shims->log("The arb ID 0x%x doesn't match the expected tx continuation ID 0x%x",
-                    arbitration_id, handle->receiving_arbitration_id);
-        }
-        return false;
-    }
-    return false;
+		
+	// we need the flowcontrol Ack here
+	if(handle->receiving_arbitration_id != arbitration_id + 0x08){
+        shims->log("Incorrect flowcontrol response");
+		handle.success = false;
+		return handle;
+	}
+	
+	// first we compute how many CAN frames are necessary
+	uint16_t num_can_frames = message->size/8;
+	
+	// send all the CAN second frames
+	for(uint16_t count = 1; count <= num_can_frames; i++){
+		isotp_send_second_frame(&shims, count, num_can_frames, handle, &message, callback);
+	}
+	
+	handle.completed = true;
+	return handle;
 }

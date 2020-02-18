3b4946cd6ac5f27cf208d267ac7907c8bc30f2ac
Prateek Prakash <59665714+PPRAKA30@users.noreply.github.com>
2020-02-18 10:34:55 -0500

    Remove leftover comment

--- src/isotp/allocate.h
+++ src/isotp/allocate.h
@@ -8,10 +8,6 @@
 extern "C" {
 #endif
 
-/* The implementation of the two functions is platform-specific and is left to
- * the user. Implementation used for testing is based on stdlib's malloc/free
- * and can be found in tests/common.c */
-
 uint8_t* allocate (size_t size);
 void free_allocated (uint8_t* data);
 
